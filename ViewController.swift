270298aadc39828fc5d10d59e9e796270ab461e9
Satomi Hanai <minoriona0310@gmail.com>
2019-07-02 08:30:28 +0900

    First commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemo
+//
+//  Created by Satomi Hanai on 2019/07/02.
+//  Copyright © 2019 Satomi Hanai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
